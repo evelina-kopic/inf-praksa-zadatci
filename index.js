5ffbc532fe6d6ac06da085e45527fb1b1963350d
evelina-kopic <92860695+evelina-kopic@users.noreply.github.com>
2022-10-09 22:37:59 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,14 @@
 /*
-Opis:
- Svima je poznat sustav "lajkanja" s Facebooka, Instagrama i drugih društvenih stranica. Ljudi mogu "lajkati" postove na blogu, slike ili druge stavke. 
- Potrebno je stvoriti tekst koji bi trebao biti prikazan pored takve stavke. 
- Implementirajte funkciju koja uzima niz koji sadrži imena ljudi kojima se neka stavka sviđa. 
- Funkcija mora vratiti tekst za prikaz kao što je prikazano u primjerima:
+  Opis:
+     Napišite funkciju pod nazivom repeater() koja uzima dva argumenta (string i broj) i vraća novi string gdje se ulazni string ponavlja toliko puta.
+   primjeri:
+     repeater("a", 5) bi trebao vratiti "aaaaa"
+     repeater("xyz", 3) bi trebao vratiti "xyzxyzxyz"
 
-  Primjeri:
-
-    likes [] //mora biti "no one likes this"
-    likes ["Petar"] //mora biti "Petar likes this"
-    likes ["Pero", "Ivan"] //mora biti "Pero and Ivan like this"
-    likes ["Robert", "Marija", "Marko"] //mora biti "Robert, Marija and Marko like this"
-    likes ["Ivan", "Pero", "Marko", "Robert"] //mora biti "Ivan, Pero and 2 others like this"
-
-  Bilješke:
-    Za više od 4 imena, broj unutar teksta "and 2 others" jednostavno se povećava.
 */
 
 //----------------RJEŠENJE ZADATAKA ------------
-function likes(imena) {
-  if(imena.lenght == 0) {
-    return "no one likes this";
-  }
-  for (let i = 0; i < imena.lenght; i++) {
-    let ostali = imena.lenght - 2;
-    if(imena.lenght == 1) {
-      return imena[0] + "likes this";
-    }
-    if(imena.lenght == 2) {
-      return imena[0] + " and " + imena[1]  + "like this";
-    }
-    if(imena.lenght == 3) {
-      return imena[0] + " , " + imena[1] + " and " + imena[2] + "like this";
-    }
-    else {
-      return imena[0] + " , " + imena[1] + " and " + ostali + "others like this";
-    }
-  }
-}
+
 
 
 
@@ -45,6 +16,5 @@ function likes(imena) {
 
 
 //---------------------------------------------------
-
 // Function Export
-module.exports = likes
+module.exports = repeater
--- index.test.js
+++ index.test.js
@@ -1,23 +1,13 @@
-const likes = require('.')
+const repeater = require('.')
 
 test('Test 1', () => {
-  expect(likes([])).toBe('no one likes this')
+  expect(repeater('a', 5)).toBe('aaaaa')
 })
 
 test('Test 2', () => {
-  expect(likes(['Petar'])).toBe('Petar likes this')
+  expect(repeater('Na', 16)).toBe('NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa')
 })
 
 test('Test 3', () => {
-  expect(likes(['Pero', 'Ivan'])).toBe('Pero and Ivan like this')
-})
-
-test('Test 4', () => {
-  expect(likes(['Robert', 'Marija', 'Marko'])).toBe('Robert, Marija and Marko like this')
-})
-
-test('Test 5', () => {
-  expect(likes(['Ivan', 'Pero', 'Marko', 'Robert'])).toBe(
-    'Ivan, Pero and 2 others like this'
-  )
+  expect(repeater('Wub ', 6)).toBe('Wub Wub Wub Wub Wub Wub ')
 })
